934707d93e345aa4623851dc65693a85088375eb
Animesh_Mukherjee <er.animesh6@hotmail.com>
2022-01-10 15:35:49 +0530

    Folder Structure Created.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='Python_Robot_Level2',
+      version='1.0',
+      description='Framework using Robot with Pyhton',
+      author='Animesh Mukherjee',
+      platforms='Windows, Mac , Linux',
+      packages=find_packages()
+)
